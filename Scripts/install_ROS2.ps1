266c9d2752ddc8715d18b5f25c533ed8cfc5a67d
jjos14 Jacob Jose <jjose14@asu.edu>
2022-02-23 23:45:21 -0700

    Added code upto install of Graphviz

--- Scripts/install_ROS2.ps1
+++ Scripts/install_ROS2.ps1
@@ -25,3 +25,44 @@ setx -m OPENSSL_CONF "C:\Program Files\OpenSSL-Win64\bin\openssl.cfg"
 #Downloading Visual Studio 2019
 
 #Invoke-WebRequest -Uri $visual_studio_2019_community_URL -OutFile $current_directory_path
+
+
+Invoke-WebRequest -Uri $URL_for_openCV -OutFile $current_directory_path
+
+Expand-Archive "$($current_directory_path)$fileName_of_openCV)" -DestinationPath $destination_for_unzipping_openCV
+
+setx -m OpenCV_DIR C:\opencv
+
+
+choco install -y cmake
+
+[Environment]::SetEnvironmentVariable("PATH", $Env:PATH + ";C:\opencv", [EnvironmentVariableTarget]::Machine)
+
+$URL_for_asio_1_12_1_nupkg = "https://github.com/ros2/choco-packages/releases/download/2020-02-24/asio.1.12.1.nupkg"
+$URL_for_bullet_2_89_0_nupkg = "https://github.com/ros2/choco-packages/releases/download/2020-02-24/bullet.2.89.0.nupkg"
+$URL_for_cunit_2_1_3_nupkg = "https://github.com/ros2/choco-packages/releases/download/2020-02-24/cunit.2.1.3.nupkg"
+$URL_for_eigen-3_3_4_nupkg = "https://github.com/ros2/choco-packages/releases/download/2020-02-24/eigen.3.3.4.nupkg"
+$URL_for_tinyxml-usestl_2_6_2_nupkg = "https://github.com/ros2/choco-packages/releases/download/2020-02-24/tinyxml-usestl.2.6.2.nupkg"
+$URL_for_tinyxml2_6_0_0_nupkg = "https://github.com/ros2/choco-packages/releases/download/2020-02-24/tinyxml2.6.0.0.nupkg"
+$URL_for_log4cxx_0_10_0_nupkg ="https://github.com/ros2/choco-packages/releases/download/2020-02-24/log4cxx.0.10.0.nupkg"
+
+
+Invoke-WebRequest -Uri $URL_for_asio_1_12_1_nupkg -OutFile $current_directory_path
+Invoke-WebRequest –Uri $URL_for_bullet_2_89_0_nupkg -OutFile $current_directory_path
+Invoke-WebRequest –Uri $URL_for_cunit_2_1_3_nupkg -OutFile $current_directory_path
+Invoke-WebRequest –Uri $URL_for_eigen-3_3_4_nupkg -OutFile $current_directory_path
+Invoke-WebRequest –Uri $URL_for_tinyxml-usestl_2_6_2_nupkg -OutFile $current_directory_path
+Invoke-WebRequest –Uri $URL_for_tinyxml2_6_0_0_nupkg -OutFile $current_directory_path
+Invoke-WebRequest –Uri $URL_for_log4cxx_0_10_0_nupkg -OutFile $current_directory_path
+
+choco install -y -s <PATH\TO\DOWNLOADS> asio cunit eigen tinyxml-usestl tinyxml2 log4cxx bullet
+
+#Installing Python dependencies
+python -m pip install -U catkin_pkg cryptography empy ifcfg lark-parser lxml netifaces numpy opencv-python pyparsing pyyaml setuptools rosdistro
+
+#RQt Dependencies
+python -m pip install -U pydot PyQt5
+
+#Installing GraphViz and appending it to Path
+choco install graphviz
+[Environment]::SetEnvironmentVariable("PATH", $Env:PATH + ";C:\Program Files\Graphviz\bin", [EnvironmentVariableTarget]::Machine)
