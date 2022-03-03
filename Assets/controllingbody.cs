71c450ed9754131f9c24b320b4b2aeb1a232cad1
nnolas27 <98365445+nnolas27@users.noreply.github.com>
2022-03-02 22:01:32 -0800

    Files for Rover structure on Unity

---
+++ Assets/controllingbody.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controllingbody : MonoBehaviour
+{
+    public Transfrom[] wheels;
+    public float motorPower = 150.0f;
+    public float maxTurn = 25.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
