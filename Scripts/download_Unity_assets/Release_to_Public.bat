406854c079a389d186826829c92f1fedde8561c1
jjos14 Jacob Jose <jjose14@asu.edu>
2022-04-01 22:19:56 -0700

    Completed batch script to download assets automatically

--- Scripts/download_Unity_assets/Release_to_Public.bat
+++ Scripts/download_Unity_assets/Release_to_Public.bat
@@ -1,4 +1,3 @@
-::setlocal EnableDelayedExpansion
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
 set vidx=0
@@ -7,17 +6,30 @@ for /F "tokens=2 delims==" %%A in (Release_to_Public-Config.txt) do (
     set var!vidx!=%%A
 )
 set var
-echo "Value of var 1 is "
-echo %var1%
-echo "Uploading Assets to GitHub"
-
 set url_of_git_repo_containing_assets_repo_URL=%var1%
+set folderContainingFilesToBeUploaded=%var2%
+set existingMainBranchRepo="old_Main_Branch-%time% of %date%"
 
-git.exe init
+echo "Files are being uploaded to %url_of_git_repo_containing_assets_repo_URL%"
+
+pushd .
+mkdir %existingMainBranchRepo%
+cd %existingMainBranchRepo%
+git clone %url_of_git_repo_containing_assets_repo_URL%
+git.exe rm -r *
 git.exe add *
-git.exe commit -m "News file uploaded at %time% of %date%"
+git.exe commit -m "Removing existing files at %time% of %date%"
+git.exe push origin HEAD:main --force
+RMDIR /s /q ".git"
+
+popd
+cd %folderContainingFilesToBeUploaded%
+git.exe init
 git.exe remote add origin %url_of_git_repo_containing_assets_repo_URL%
 git.exe pull --set-upstream origin main
+git.exe rm -r *
+git.exe add *
+git.exe commit -m "New files uploaded at %time% of %date%"
 git.exe push origin HEAD:main --force
 RMDIR /s /q ".git"
 
